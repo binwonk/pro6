923a385f5ba8baed23f692aaf920f97dd95f370c
binwonk <80070770+binwonk@users.noreply.github.com>
2023-06-10 16:30:23 +0100

    Update 3082707367.lua

--- games/1111920082/3082707367.lua
+++ games/1111920082/3082707367.lua
@@ -18,7 +18,7 @@ local ReplicatedStorage = game:GetService("ReplicatedStorage")
 
 -- LOCAL VARIABLES HERE
 local Player = Players.LocalPlayer
-local Modules = ReplicatedStorage.Modules
+local Modules = ReplicatedStorage:WaitForChild("Modules")
 local Remote = Modules:WaitForChild("Network"):WaitForChild("RemoteEvent")
 
 local ChaosFunctions = loadstring(game:HttpGet("https://raw.githubusercontent.com/binwonk/pro6/main/misc/ChaosFunctions2.lua"))()
@@ -38,13 +38,13 @@ Options.KillPlayer:OnChanged(function(value)
 	local KillPlayer = ChaosFunctions.stringToPlayer(value)
     print(tostring(ChaosFunctions.checkType(KillPlayer)))
 	print(tostring(KillPlayer:GetFullName()))
-	if KillPlayer.Character and KillPlayer.Character:FindFirstChild("LeftHand") then
+	if KillPlayer.Character and KillPlayer.Character:FindFirstChild("Head") then
 		for i = 1, 6 do
 			local args = {
 				[1] = "HandleDamage",
 				[2] = {
 					["Character"] = KillPlayer.Character,
-					["Hit"] = KillPlayer.Character.LeftHand,
+					["Hit"] = KillPlayer.Character.Head,
 					["Type"] = "Normal",
 					["Norm"] = Vector3.new(0.7755845785140991, 0.035323500633239746, -0.6302545070648193),
 					["Pos"] = Vector3.new(-731.4620971679688, -108.57284545898438, -495.60650634765625),
