a6b8b8b6a991675e608d0bdbdc577c01bcb3b9d3
binwonk <80070770+binwonk@users.noreply.github.com>
2023-06-11 21:03:34 +0100

    Update 3082707367.lua

--- games/1111920082/3082707367.lua
+++ games/1111920082/3082707367.lua
@@ -157,7 +157,7 @@ Options.BringPlayer:OnChanged(function(value)
 				v114.D = 100;
 				local u34 = RunService.Stepped:Connect(function()
 					if Player.Character:FindFirstChild("HumanoidRootPart") and v109 then
-						v114.CFrame = Vector3.new(Player.Character:FindFirstChild("HumanoidRootPart").CFrame * CFrame.new(0,0,-3))
+						v114.Position = Vector3.new(Player.Character:FindFirstChild("HumanoidRootPart").CFrame * CFrame.new(0,0,-3))
 					end
 				end)
 				wait(ROWizardValues["BringPlayerTimeValue"]);
